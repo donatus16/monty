6587ca06d469be09fb77e64d8f1acd75744c5658
donatus16 <donatuskto@gmail.com>
2023-06-22 17:52:23 -0700

    swap.c

---
+++ monty.h
@@ -0,0 +1,19 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+
+
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+void f_pint(stack_t **head, unsigned int number);
+void f_push(stack_t **head, unsigned int number);
+
+#endif
